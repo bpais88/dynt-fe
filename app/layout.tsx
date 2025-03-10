33add8a35998de789f5675a1bccf84bcbd1adaaa
Bruno Pais <bruno.pais88@gmail.com>
2025-03-11 00:18:56 +0700

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type React from "react";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,6 +25,7 @@ export default function RootLayout({
           {children}
           {/* <Toaster position="top-right" /> */}
         </div>
+        <Analytics />
       </body>
     </html>
   );
