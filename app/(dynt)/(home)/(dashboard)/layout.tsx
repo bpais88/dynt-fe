f6dd727799c0be7ec0b59e44a4156901cb65fa8d
Prabin Acharya <acharyaprabin101@gmail.com>
2025-03-14 01:35:33 +0545

    redirect to /login if not signed in, optional chaining to prevent null errors

--- app/(dynt)/(home)/(dashboard)/layout.tsx
+++ app/(dynt)/(home)/(dashboard)/layout.tsx
@@ -1,15 +1,31 @@
 "use client";
 
 import { MainNavSidebar } from "@/components/main-nav-sidebar";
+import { useOrganization } from "@/context/OrganizationContext";
 import { useUser } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
 import type React from "react";
+import { useEffect } from "react";
 
 export default function InvoicesBillsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { userId } = useUser<true>();
+  const { user, session, userId } = useUser();
+  const { organization } = useOrganization();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.push("/login");
+    }
+
+    if (!userId) {
+      router.push("/login");
+    }
+  }, [organization, router, session, user, userId]);
 
   return (
     <>
