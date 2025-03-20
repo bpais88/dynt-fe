5f840747a8f4a8563eaaa4905b0da1c73f6e6062
Prabin Acharya <acharyaprabin101@gmail.com>
2025-03-21 01:47:49 +0545

    added create bill button in invoices-bills page

--- app/(dynt)/(home)/(dashboard)/invoices-bills/page.tsx
+++ app/(dynt)/(home)/(dashboard)/invoices-bills/page.tsx
@@ -1,21 +1,20 @@
 "use client";
-
 import { InvoicesBillsList } from "@/components/invoices-bills-list";
-
+import { Button } from "@/components/ui/button";
 import { useOrganization } from "@/context/OrganizationContext";
 import { api, RouterInputs } from "@/utils/trpc";
 import { endOfYear, startOfYear, subYears } from "date-fns";
-
+import { PlusIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 type BillFilter = RouterInputs["bills"]["bills"]["filters"];
 type InvoiceFilter = RouterInputs["invoices"]["invoices"]["filters"];
 
 export default function InvoicesBillsPage() {
+  const router = useRouter();
   const { organizationId = "" } = useOrganization<true>();
-
   const [filters, setFilters] = useState<InvoiceFilter | BillFilter>(null);
-
   const [sorting, setSorting] = useState<Sorting>({
     createdAt: "desc",
   });
@@ -41,13 +40,42 @@ export default function InvoicesBillsPage() {
   console.log("+++++++++++++++++++++");
   console.log(invoices, bills, organizationId, params);
 
+  const handleCreateBill = () => {
+    router.push("/invoices-bills/bills/create");
+  };
+
+  const handleCreateInvoice = () => {
+    router.push("/invoices-bills/invoices/create");
+  };
+
   return (
     <>
       <div className="px-20 py-12">
-        <h2 className="text-2xl font-bold">Invoices & Bills</h2>
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-2xl font-bold">Invoices & Bills</h2>
+          <div className="flex gap-3">
+            {/* <Button
+              onClick={handleCreateInvoice}
+              variant="outline"
+              className="flex items-center gap-1 cursor-pointer border-black text-black hover:bg-gray-50"
+            >
+              <PlusIcon size={14} />
+              Create New Invoice
+            </Button> */}
+            <Button
+              onClick={handleCreateBill}
+              variant="outline"
+              className="flex items-center gap-1 cursor-pointer border-black text-black hover:bg-gray-50"
+            >
+              <PlusIcon size={14} />
+              Create New Bill
+            </Button>
+          </div>
+        </div>
+
         {isLoadingInvoices || isLoadingBills ? (
           <div className="flex py-8 pt-36">
-            <div className="animate-spin rounded-full h-8 w-8  m-auto border-t-2 border-b-2 border-primary"></div>
+            <div className="animate-spin rounded-full h-8 w-8 m-auto border-t-2 border-b-2 border-primary"></div>
           </div>
         ) : (
           <InvoicesBillsList invoices={invoices} bills={bills} />
